5a656f7028e30ca7a03b0a4466bf1dffd28f80b8
Jay Houn <jayhoun@gmail.com>
2018-08-07 09:20:39 -1000

    updated questions

--- exercises.js
+++ exercises.js
@@ -1,34 +1,10 @@
-/* #doubleArray
- *
- * Takes in an array of numbers and returns an array with each element doubled
- *
- * @param {Array} // [1,2,3]
- * @return {Array} // [2,4,6]
- */
-var doubleArray = function(arr) {
-  var result = [];
-  for (var i = 0; i < arr.length; i++) {
-    result.push(arr[i] * 2) 
-  }
-  return result
-}
-
-/* #sumArrays
- *
- * Takes in two arrays of numbers and returns the sum of both arrays.
- *
- * @param {Array}
- * @param {Array}
- * @return {Bool}
- */
-var sumArrays;
-
 /* #stringCount
  *
  * Takes in a string and returns the length of the string.
  *
  * @param {String}
  * @return {Number}
+ * 
  */
 var stringCount;
 
@@ -75,24 +51,7 @@ var countAllStrings;
  * @param {Object}
  * @return {Array}
  */
-var convertToArray;
 
-/* #objectSize
- *
- * Takes in an object and returns the number of key value pairs in the object.
- *
- * @param {Object}
- * @return {Number}
- */
-var objectSize;
-
-/* #createZeroFilledArray
- *
- * Takes in a number and fills an array with that number of zeroes.
- *
- * @param {Number}
- * @return {Zero}
- */
 var createZeroFilledArray;
 
 /* #poppedArray
@@ -167,6 +126,15 @@ var sumAllPositive;
  */
 var stringCountBelowThree;
 
+/* #doubleArray
+ *
+ * Takes in an array of numbers and returns an array with each element doubled
+ *
+ * @param {Array} // [1,2,3]
+ * @return {Array} // [2,4,6]
+ */
+var doubleArray;
+
 /* #countObjects
  *
  * Takes in an array of objects and returns the amount of objects in the array.
@@ -183,202 +151,14 @@ var countObjects;
  * @param {Object}
  * @return {Array}
  */
-var getObjectKeys;
-
-/* #getObjectValues
- *
- * Takes in an object and returns an array of all the object's values.
- *
- * @param {Object}
- * @return {Array}
- */
-var getObjectValues;
-
-/* #makeObject
- *
- * Takes in two arguments 'key' and 'value' and returns a single key-value pair in an object.
- *
- * @param {String}
- * @param {String}
- * @return {Object}
- */
-var makeObject;
-
-/* #makeObjectReverse
- *
- * Takes in two arguments 'value' and 'key' and returns a single key-value pair in an object.
- *
- * @param {String}
- * @param {String}
- * @return {Bool}
- */
-var makeObjectReverse;
-
-/* #tupleToObject
- *
- * Takes in a tuple (an array with two elements) and returns it into a single key-value pair in an object.
- *
- * @param {Array}
- * @return {Object}
- */
-var tupleToObject;
-
-/* #tupleToObjectReverse
- *
- * Takes in a tuple and returns it into a single key-value pair with second tuple element as key and first tuple element as value.
- *
- * @param {Array}
- * @return {Object}
- */
-var tupleToObjectReverse;
-
-/* #strToKeys
- *
- * Takes in an array of strings and returns an object with all string elements as the object's keys and all values set to 0.
- *
- * @param {Array}
- * @return {Object}
- */
-var strToKeys;
-
-/* #getValues
- *
- * Takes in an object and returns an array of all the object's values.
- *
- * @param {Object}
- * @return {Array}
- */
-var getValues;
-
-/* #getKeys
- *
- * Takes in an object and returns an array of the object's keys.
- *
- * @param {Object}
- * @return {Array}
- */
-var getKeys;
-
-/* #objectToArray
- *
- * Takes in an object and returns an array of tuples where each tuple has 
- * the object's key as element 0 and object's value as element 1.
- *
- * @param {Object}
- * @return {Array}
- */
-var objectToArray;
-
-/* #arrayToObject
- *
- * takes in an array and returns an object with keys set to the elements in the array and
- * all values set to false.
- *
- * @param {Array}
- * @return {Object}
- */
-var arrayToObject;
-
-/* #arraysToObject
- *
- * takes in two arrays, the first array elements will be keys of an object and second array elements 
- * will be values of an object.
- *
- * @param {Array}
- * @param {Array}
- * @return {Object}
- */
-var arraysToObject;
-
-/* #objectsToTuples
- *
- * takes in two objects and returns an array of tuples of the key value pairs of all objects in both arrays.
- *
- * @param {Object}
- * @param {Object}
- * @return {Array}
- */
-var objectsToTuples;
-
-/* #mapArrayValues
- *
- * takes in an array of strings and returns an object with keys of the array elements and values all set to True.
- *
- * @param {Array}
- * @return {Object}
- */
-var mapArrayValues;
-
-/* #mapStringCounts
- *
- * takes in an array of strings and returns an object with key names set to the elements in the array.
- * If the character count of the key name is greater than or equal to 5 set the value to true,
- * otherwise set to false.
- *
- * @param {Array}
- * @return {Object}
- */
-var mapStringCounts;
-
-/* #arrayToObjectNums
- *
- * takes in an array of numbers and returns an object with keys set to 
- * each element of the array and all values set to true.
- *
- * @param {Array}
- * @return {Object}
- */
-var arrayToObjectNums;
-
-/* #stringToKeys
- *
- * takes in a string and returns an object whos keys are each letter of the string and all values set to true.
- * 
- * @param {String}
- * @return {Object}
- */
-var stringToKeys;
-
-/* #charCountMap
- *
- * takes in an array of strings and returns an object with keys set to each element of the array 
- * and values set to the character count of each key name.
- *
- * @param {Array}
- * @return {Object}
- */
-var charCountMap;
-
-/* #frequencyMap
- *
- * takes in an array of strings and returns an object with the string as the key and the number of occurences as the value.
- *
- * @param {String}
- * @return {Bool}
- */
-var frequencyMap;
-
-/* #tupleConvertToObject
- *
- * takes in an array of tuples and and returns an object whos keys are 
- * the first element of the tuples and values are second element of the tuples.
- *
- * @param {String}
- * @return {Bool}
- */
-var tupleConvertToObject;
-
 
 module.exports = {
-  doubleArray: null,
-  sumArrays: null, 
+  doubleArray: doubleArray,
   stringCount: null,
   arrayLength: null,
   countAll: null,
   countStrings: null,
   countAllStrings: null,
-  convertToArray: null,
-  objectSize: null,
   createZeroFilledArray: null,
   poppedArray: null,
   splitString: null,
@@ -388,25 +168,5 @@ module.exports = {
   multiplyAll: null,
   sumAllPositive: null,
   stringCountBelowThree: null,
-  countObjects: null,
-  getObjectKeys: null,
-  getObjectValues: null,
-  makeObject: null,
-  makeObjectReverse: null,
-  tupleToObject: null,
-  tupleToObjectReverse: null,
-  strToKeys: null,
-  getValues: null,
-  getKeys: null,
-  objectToArray: null,
-  arrayToObject: null,
-  arraysToObject: null,
-  objectsToTuples: null,
-  mapArrayValues: null,
-  mapStringCounts: null,
-  arrayToObjectNums: null,
-  stringToKeys: null,
-  charCountMap: null,
-  frequencyMap: null,
-  tupleConvertToObject: null
-}
+  countObjects: null
+};
